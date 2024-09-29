03026e8b74f4e5fe96c77bbb72aa68217fcfa56a
Sean Swanson <seancswan@gmail.com>
2024-09-29 13:48:31 -0700

    Dialing in the front page and setting up for internal pages.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import sitemap from '@astrojs/sitemap';
-import vercel from '@astrojs/vercel/serverless';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
+import vercel from "@astrojs/vercel/serverless";
+import icon from "astro-icon";
 
-import partytown from '@astrojs/partytown';
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), partytown()],
-  output: 'server',
+  integrations: [tailwind(), icon(), sitemap(), partytown()],
+  output: "server",
   adapter: vercel(),
 });
